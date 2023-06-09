42d5b1ebd1929f89b538b87581e58b061faac4e0
Dhanilow15 <dhanilow14@gmail.com>
2023-06-09 14:27:24 -0300

    feat (*) branch and bound class in development 09.06.2023

--- final_project_OR/branch_and_bound/branch_and_bound.py
+++ final_project_OR/branch_and_bound/branch_and_bound.py
@@ -1,26 +1,20 @@
 from mip import *
 
-from utils.read_problem_example_file import read_problem_example_file
+from final_project_OR.branch_and_bound.preprocessing.modeling import OptmModel
 
 
-def model_problem():
-    # setting problem info object
-    optm_problem = read_problem_example_file(file_name='problem_example_1')
+class BranchAndBound():
+    def __init__(self, initial_model: Model):
+        self.initial_model = initial_model
+        self.initial_solution = self.initial_model.optimize()
+        self.a = 1
 
-    # modeling.py the problem
-    optm_model = Model(sense=MAXIMIZE, solver_name=CBC)
+    def daada(self):
+        pass
 
-    # creating vars dictionary
-    x = {i + 1: optm_model.add_var(var_type=CONTINUOUS, name=f"x_{i + 1}")
-         for i in range(optm_problem.num_vars)}
 
-    # creating restrictions
-    for coefficients in optm_problem.restrictions.values():
-        optm_model += xsum(c*x[index+1] for index, c in enumerate(coefficients[:-1])) <= coefficients[-1]
+if __name__ == '__main__':
+    my_model = OptmModel(file_name='problem_example_1')
+    my_bb = BranchAndBound(initial_model=my_model.model_problem())
 
-    optm_model.write("model.lp")  # salva modelo em arquivo
-    with open("model.lp", "r") as f:  # lê e exibe conteúdo do arquivo
-        print(f.read())
-
-if __name__ == "__main__":
-    model_problem()
+    my_bb.daada()
