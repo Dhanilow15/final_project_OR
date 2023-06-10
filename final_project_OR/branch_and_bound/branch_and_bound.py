17f1ddbf30a630e461d295502d8ebb995120e3ab
Dhanilow15 <dhanilow14@gmail.com>
2023-06-10 11:17:26 -0300

    feat (branch_and_bound.py) filters 09.06.2023

--- final_project_OR/branch_and_bound/branch_and_bound.py
+++ final_project_OR/branch_and_bound/branch_and_bound.py
@@ -8,10 +8,42 @@ class BranchAndBound():
         # saving initial problem info, without screen printing
         self.initial_model = initial_model
         self.initial_model.verbose = False
-        self.initial_vars = initial_vars
+        self.vars = initial_vars
         self.initial_solution = self.initial_model.optimize()
+        self.primal_limit = 0
 
+    def filter_feasibility(self, solution_satus: OptimizationStatus) -> bool:
+        """
+        Feasibility filter for branch and bound
+        Args:
+            solution_satus (OptimizationStatus): optimization status of a model solution
+        Returns:
+            (bool): True if solution is feasible, otherwise False
+        """
+        return not solution_satus.INFEASIBLE
 
+    def filter_integrality(self, solution_vars: list[float]) -> bool:
+        """
+        Integrality filter for branch and bound
+        Args:
+            solution_vars (list[float]): list with the var values for a model solution
+        Returns:
+            (bool): True is every variable is a whole number, otherwise False
+        """
+        return all(var.is_integer() for var in solution_vars)
+
+    def filter_inferior_solution(self, solution_value: float) -> bool:
+        """
+        Inferior value solution filter for branch and bound
+        Args:
+            solution_value (float): solution value of a model
+        Returns:
+            (bool): True if solution is greater than primal limit, otherwise False
+        """
+        return solution_value > self.primal_limit
+
+    def execute_branch_and_bound(self):
+        pass
 
 if __name__ == '__main__':
     my_model = OptmModel(file_name='problem_example_1')
@@ -19,4 +51,5 @@ if __name__ == '__main__':
     print('-----------------')
     print(my_bb.initial_solution)
     print(my_bb.initial_model.objective_value)
-    print(my_bb.initial_vars)
+    for v in range(len(my_bb.vars)):
+        print(my_bb.vars[v+1].x)
