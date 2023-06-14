e196b5f2c9bfd6415c3074d758395e4f0b32534d
Dhanilow15 <dhanilow14@gmail.com>
2023-06-14 14:47:56 -0300

    new (*) setup 14.06.2023

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='final_project_OR',
+    version='1.0.0',
+    author='Your Name',
+    author_email='dhanilow14@gmail.com',
+    description='Package for OR discipline final project',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
