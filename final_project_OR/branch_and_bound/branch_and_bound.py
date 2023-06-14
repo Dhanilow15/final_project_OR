dbc356b6972b0abcffdbc38c4ce0d220937e02ec
Dhanilow15 <dhanilow14@gmail.com>
2023-06-14 12:12:52 -0300

    bugfix (branch_and_bound.py) recursion bug fix 14.06.2023

--- final_project_OR/branch_and_bound/branch_and_bound.py
+++ final_project_OR/branch_and_bound/branch_and_bound.py
@@ -4,12 +4,13 @@ from mip import Model, OptimizationStatus, Var
 
 
 class BranchAndBound:
-    def __init__(self, initial_model: Model, initial_vars: Var):
+    def __init__(self, initial_model: Model, initial_vars: Var, num_initial_restrictions: int):
         # saving initial problem info, without screen printing
         self.initial_model = initial_model
         self.initial_model.verbose = False
         self.vars = initial_vars
         self.initial_solution = self.initial_model.optimize()
+        self.num_initial_restrictions = num_initial_restrictions
         self.primal_limit = 0
 
     @staticmethod
@@ -55,12 +56,16 @@ class BranchAndBound:
         """
         return solution_value < self.primal_limit
 
-    def execute_branch_and_bound(self, start_model: Model):
+    def execute_branch_and_bound(self, start_model: Model, n_restrs: int):
+        # solving
         if start_model != self.initial_model:
             solution = start_model.optimize()
             # checking feasibility
             if self.filter_feasibility(solution_satus=solution):
                 return
+            # start_model.write("model_{self.file_name}.lp")  # saves model on archive
+            # with open("model_{self.file_name}.lp", "r") as f:  # reads and exhibit model
+            #      print(f.read())
 
         # getting solution vars for this iteration
         solution_vars = [self.vars[i + 1].x for i in range(len(self.vars))]
@@ -73,6 +78,7 @@ class BranchAndBound:
         # setting new primal limit
         elif not decimal_vars:
             self.primal_limit = start_model.objective_value
+            return
 
         # searching in depth
         for var in decimal_vars:
@@ -80,12 +86,21 @@ class BranchAndBound:
             border_integers = self.get_border_integers_for_float(solution_vars[var-1])
             # branching and bounding
             for index, integer in enumerate(border_integers):
+                # setting relevant info
                 relaxated_model = copy.copy(start_model)
+                restrs_list = list(relaxated_model.constrs)
+
+                # removing exceeding restrs
+                if len(restrs_list) > n_restrs and n_restrs == self.num_initial_restrictions:
+                    exceeding_restrs = restrs_list[n_restrs:]
+                    for restr in exceeding_restrs:
+                        relaxated_model.remove(restr)
+
                 # restriction to be lesser than bottom limit
-                if index:
-                    relaxated_model += self.vars[var] >= integer
+                if index == 1:
+                    relaxated_model.add_constr(self.vars[var] >= integer)
                 # greater than upper limit
                 else:
                     relaxated_model += self.vars[var] <= integer
                 # recursive call
-                self.execute_branch_and_bound(start_model=relaxated_model)
+                self.execute_branch_and_bound(start_model=relaxated_model, n_restrs=n_restrs+1)
