fddecc32ffe0bbed1325e58988e82bc409ec529e
Dhanilow15 <dhanilow14@gmail.com>
2023-06-14 14:06:47 -0300

    chore (*) docstrings and organization 14.06.2023

--- final_project_OR/branch_and_bound/preprocessing/modeling.py
+++ final_project_OR/branch_and_bound/preprocessing/modeling.py
@@ -3,6 +3,7 @@ from mip import Model, MAXIMIZE, CBC, xsum, CONTINUOUS
 from final_project_OR.branch_and_bound.utils.read_problem_example_file import OptmProblem
 from final_project_OR.branch_and_bound.utils.read_problem_example_file import read_problem_example_file
 
+
 class OptmModel:
     def __init__(self, file_name: str):
         # setting problem info object
@@ -23,14 +24,15 @@ class OptmModel:
 
         # creating vars dictionary, with linear relaxation
         self.vars = {i + 1: optm_model.add_var(var_type=CONTINUOUS, name=f"x_{i + 1}", lb=0, ub=1)
-             for i in range(self.problem.num_vars)}
+                     for i in range(self.problem.num_vars)}
 
         # creating target function
-        optm_model.objective = xsum(c*self.vars[index+1] for index, c in enumerate(self.problem.obj_function))
+        optm_model.objective = xsum(c * self.vars[index + 1] for index, c in enumerate(self.problem.obj_function))
 
         # creating restrictions
         for coefficients in self.problem.restrictions.values():
-            optm_model += xsum(c * self.vars[index + 1] for index, c in enumerate(coefficients[:-1])) <= coefficients[-1]
+            optm_model += xsum(c * self.vars[index + 1] for index, c in enumerate(coefficients[:-1])) <= coefficients[
+                -1]
 
         optm_model.write(f"model_{self.file_name}.lp")  # saves model on archive
         # with open(f"model_{self.file_name}.lp", "r") as f:  # reads and exhibit model
