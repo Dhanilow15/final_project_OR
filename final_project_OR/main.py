401d4d547e99e17531d598b4a881f6ffddd2e7d9
Dhanilow15 <dhanilow14@gmail.com>
2023-06-14 12:13:14 -0300

    chore (main.py) comment removed 14.06.2023

--- final_project_OR/main.py
+++ final_project_OR/main.py
@@ -1,15 +1,17 @@
 from final_project_OR.branch_and_bound.branch_and_bound import BranchAndBound
 from final_project_OR.branch_and_bound.preprocessing.modeling import OptmModel
 
+
 def solve_problem(file_name: str):
     solve_model = OptmModel(file_name=file_name)
     solve_bb = BranchAndBound(initial_model=solve_model.model_problem(),
-                           initial_vars=solve_model.vars)
-    solve_bb.execute_branch_and_bound(start_model=solve_bb.initial_model)
+                              initial_vars=solve_model.vars,
+                              num_initial_restrictions=solve_model.problem.num_restrictions)
+    solve_bb.execute_branch_and_bound(start_model=solve_bb.initial_model, n_restrs=solve_model.problem.num_restrictions)
 
     print(f'Optimal integer solution for {file_name} is {solve_bb.primal_limit}')
 
+
 if __name__ == '__main__':
-    # for i in range(1,5):
-    #     solve_problem(file_name=f'problem_example_{i}')
-    solve_problem(file_name=f'problem_example_{4}')
+    for i in range(1,5):
+        solve_problem(file_name=f'problem_example_{i}')
